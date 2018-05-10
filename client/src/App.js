26e7a77bbe2b36b574c779887c048673060891b7
Shirley Xu <xshirl525@gmail.com>
2018-05-10 15:35:53 -0400

    finished search bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import RestaurantList from './components/RestaurantList'
+import RestaurantList from './components/RestaurantList';
+import RestaurantFilter from './components/RestaurantFilter';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      restaurants: []
+      restaurants: [],
+
     }
+
+    this.handleFilterSubmit = this.handleFilterSubmit.bind(this);
   }
 
+
+
+  fetchRestaurants(term,location) {
+    fetch(`/restaurants/${term}/${location}`)
+    .then(resp=> {
+      if(!resp.ok) throw new Error(resp.statusMessage);
+      return resp.json();
+    })
+    .then(respBody => {
+      this.setState({
+        restaurants: respBody.data.businesses
+      })
+    })
+  }
+
+ handleFilterSubmit(options) {
+  console.log(options);
+  this.fetchRestaurants(options.term, options.location);
+ }
+
+  componentDidMount() {
+
+  }
   render() {
     return (
       <Router>
       <div className="App">
       <p>hello world</p>
-
-
+      <RestaurantFilter onSubmit = {this.handleFilterSubmit} term={this.state.term} location={this.state.location} />
       </div>
 
       </Router>
