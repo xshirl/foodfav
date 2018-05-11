04f260f0e77bdade146c6396a0d67551ca6ba4ea
Shirley Xu <xshirl525@gmail.com>
2018-05-11 08:31:53 -0400

    added restaurant list

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -3,5 +3,18 @@ import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
 
 export default function RestaurantList(props) {
+  return (
+    <div class="list">
+      {props.restaurants.map(restaurant => (
+        <div class="restaurant">
+        <img src={restaurant.image_url} height='300' />
+        <h3> {restaurant.name} </h3>
+        <h4>Location: {restaurant.location.address1} {restaurant.location.city} {restaurant.location.zip_code} </h4>
+        <h5> Rating: {restaurant.rating} </h5>
+        <h5> Price range: {restaurant.price} </h5>
+        </div>
+        ))}
+    </div>
+    )
+  }
 
-}
